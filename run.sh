d4a55bfbc3067a68c38034674d27bb857c750320
xiaoz <xiaoz93@outlook.com>
2022-07-18 17:19:08 +0800

    fix bug

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ WEBMAN_DIR=/data/wwwroot/webman
 #判定启动文件是否存在，不存在，说明是一个新的容器
 if [ !-f ${WEBMAN_DIR}/start.php ]
 then
-	cp -ar /data/wwwroot/default ${WEBMAN_DIR}
+	cp -ar /data/wwwroot/default/* ${WEBMAN_DIR}/
 fi
 
 #启动
