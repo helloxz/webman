5558b44f3ea92137157e49aefe75838f5402ed01
xiaoz <xiaoz93@outlook.com>
2022-08-29 16:10:52 +0800

    add pdo_mysql

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # webman
 PHP框架webman的Docker镜像
+
+## Docker运行
+
+```bash
+docker run -d -p 8787:8787 -v /data/webman:/data/wwwroot/webman helloz/webman:latest
+```
+
+## docker-compose运行
+
+```yaml
+version: '3.3'
+services:
+    webman:
+        network_mode: "host"
+        ports:
+            - '8787:8787'
+        volumes:
+            - './data:/data/wwwroot/webman'
+        image: 'helloz/webman:latest'
+```
+
--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ date
 #为了精简镜像，可以将tzdata删除了
 apk del tzdata
 #安装PHP
-apk add php7 php7-xml php7-xmlrpc php7-openssl php7-posix php7-pcntl php7-sqlite3 php7-pdo_sqlite php7-curl php7-json php7-session php7-phar php7-iconv php7-mbstring php7-fileinfo php7-exif php7-redis php7-gd php7-pecl-imagick-dev php7-pecl-imagick php7-mysqlnd php7-mysqli php7-ctype composer
+apk add php7 php7-pdo_mysql php7-xml php7-xmlrpc php7-openssl php7-posix php7-pcntl php7-sqlite3 php7-pdo_sqlite php7-curl php7-json php7-session php7-phar php7-iconv php7-mbstring php7-fileinfo php7-exif php7-redis php7-gd php7-pecl-imagick-dev php7-pecl-imagick php7-mysqlnd php7-mysqli php7-ctype composer
 
 #更名PHP配置文件
 mv /etc/php7/php.ini /etc/php7/php.ini.bak
